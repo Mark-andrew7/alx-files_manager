8bbd306d24ad54805d5575565ab40c4b28eef77e
Mark-andrew7 <kamaumark19@gmail.com>
2024-09-05 03:53:13 +0300

    Added new endpoint for creating a new user

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,6 @@ router.get('/stats', AppController.getStats);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
+router.post('/users', UsersController.postNew);
 
 export default router;
